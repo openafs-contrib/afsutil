b37c2f98ab287014b93054bd754398d57664f9b2
Michael Meffie <mmeffie@sinenomine.net>
2018-09-07 16:48:15 -0400

    rpm: long description fallback
    When building rpms, the README.rst path may not be available, so
    fall back to the short description instead of failing to build
    the rpm package.

--- setup.py
+++ setup.py
@@ -4,13 +4,20 @@ except ImportError:
     # Fallback to the standard library distutils.
     from distutils.core import setup
 
-exec(open('afsutil/__version__.py').read())
+exec(open('afsutil/__version__.py').read()) # sets VERSION
+
+description='Utilities to setup OpenAFS clients and servers'
+try:
+    long_description = open('README.rst').read()
+except:
+    # Fallback if readme is not found.
+    long_description = description
 
 setup(
     name='afsutil',
     version=VERSION,
-    description='Utilities to setup OpenAFS clients and servers',
-    long_description=open('README.rst').read(),
+    description=description,
+    long_description=long_description,
     author='Michael Meffie',
     author_email='mmeffie@sinenomine.net',
     url='https://github.com/openafs-contrib/afsutil',
